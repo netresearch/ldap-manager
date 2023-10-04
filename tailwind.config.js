db3a392b8a4b8f61f193ebde83086ec1041e2aa9
DevMiner <devminer@devminer.xyz>
2023-10-04 14:33:27 +0200

    impl: nicer navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 import plugin from "tailwindcss/plugin";
 
+const goExtractor = (postfix = "Classes") => {
+  /**
+   * @param {string} content
+   * @returns {[]string}
+   */
+  return (content) => {
+    const regex = new RegExp(`^const .*${postfix} = "(.+)";?`, "gm");
+
+    const matches = regex.exec(content);
+    if (!matches) return [];
+
+    return matches[1].split(" ");
+  };
+};
+
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["internal/web/layouts/*.html", "internal/web/views/*.html"],
+  content: {
+    files: ["internal/web/layouts/*.html", "internal/web/views/*.html", "internal/web/templates.go"],
+    extract: {
+      go: goExtractor("Classes")
+    }
+  },
   plugins: [
     plugin(({ addVariant }) => {
       addVariant("hocus", ["&:hover", "&:focus"]);
@@ -10,8 +30,7 @@ const config = {
   ],
   theme: {
     extend: {}
-  },
-  safelist: ["bg-gray-700"]
+  }
 };
 
 export default config;
